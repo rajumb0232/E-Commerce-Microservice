2ef4b7b293a5f06ce324b6b5021b2d287c3e1675
rajumb0232 <rajugowda0232@gmail.com>
2025-02-14 13:16:26 +0530

    working order-service, creating demo interservice communication using RestTemplate

---
+++ order-service/src/main/java/com/example/order/repository/CartItemRepository.java
@@ -0,0 +1,7 @@
+package com.example.order.repository;
+
+import com.example.order.model.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+}
---
+++ order-service/src/main/java/com/example/order/service/OrderService.java
@@ -0,0 +1,11 @@
+package com.example.order.service;
+
+import com.example.order.repository.OrderRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class OrderService {
+    private OrderRepository orderRepository;
+}
