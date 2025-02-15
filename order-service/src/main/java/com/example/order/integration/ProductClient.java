3e5f9a47252d412ef0df277def463e3ead5702d7
rajumb0232 <rajugowda0232@gmail.com>
2025-02-15 11:39:11 +0530

    externalizing the product client configuration for better maintainability

--- order-service/src/main/java/com/example/order/integration/ProductClient.java
+++ order-service/src/main/java/com/example/order/integration/ProductClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "product-service", url = "http://localhost:7002/api/v1/ps")
+@FeignClient(name = "${app.client.product.name}", url = "${app.client.product.url}")
 public interface ProductClient {
 
     @GetMapping("/products/{id}/availability")
