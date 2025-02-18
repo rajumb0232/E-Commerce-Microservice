ed59fd8ac48e29c49feb3ac186ac105949a8a072
rajumb0232 <rajugowda0232@gmail.com>
2025-02-18 14:03:33 +0530

    handling exception if the cartItem's are not found, and implemented logging

--- order-service/src/main/java/com/example/order/service/OrderService.java
+++ order-service/src/main/java/com/example/order/service/OrderService.java
@@ -1,6 +1,8 @@
 package com.example.order.service;
 
 import com.example.order.exception.ProductOutOfStockException;
+import com.example.order.exception.UnorderedCartItemsNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.AllArgsConstructor;
@@ -24,6 +26,7 @@ import com.example.order.repository.OrderRepository;
 @Service
 @AllArgsConstructor
 @Transactional
+@Slf4j
 public class OrderService {
 
     private final OrderRepository orderRepository;
@@ -35,7 +38,8 @@ public class OrderService {
     // TODO: The Logic should be updated to validate the user's ownership and then create the order.
 
     public OrderResponse createOrder() {
-        List<CartItem> cartItems = cartItemRepository.findAllByOrdered(false);
+        log.info("Attempting to create a new order...");
+        List<CartItem> cartItems = getUnOrderedCartItems();
 
         /* Extracting the total amount from the cart items
          * */
@@ -55,16 +59,29 @@ public class OrderService {
         List<CartItemResponse> cartItemResponses = mapToCartItemResponseList(cartItems);
 
         // returning the OrderResponse object.
+        log.info("Order created successfully.");
         return getOrderResponse(order, cartItemResponses);
     }
 
+    private List<CartItem> getUnOrderedCartItems() {
+        log.info("Fetching un-ordered cart items...");
+        List<CartItem> items = cartItemRepository.findAllByOrdered(false);
+        if (items.isEmpty()) {
+            log.error("No un-ordered cart items found.");
+            throw new UnorderedCartItemsNotFoundException("No un-ordered cart items found.");
+        }
+        return items;
+    }
+
     private OrderResponse getOrderResponse(Order order, List<CartItemResponse> cartItemResponses) {
+        log.debug("Creating OrderResponse object...");
         OrderResponse orderResponse = orderMapper.mapToOrderResponse(order);
         orderResponse.setOrderItems(cartItemResponses);
         return orderResponse;
     }
 
     private List<CartItemResponse> mapToCartItemResponseList(List<CartItem> cartItems) {
+        log.debug("Mapping cart items to CartItemResponse objects...");
         return cartItems.stream()
                 .map(item -> {
                     Product product = productClient.getProductById(item.getProductId());
@@ -76,6 +93,7 @@ public class OrderService {
     }
 
     private void UpdateCartItemAsOrdered(List<CartItem> cartItems, Order order) {
+        log.info("Updating cart items as ordered...");
         cartItems.forEach(item -> {
             item.setOrdered(true);
             item.setOrder(order);
@@ -84,11 +102,14 @@ public class OrderService {
     }
 
     private BigDecimal ExtractTotalAmountFromItemList(List<CartItem> cartItems) {
+        log.debug("Extracting total amount from cart items...");
+        log.debug("Validating cart items for stock availability...");
         return cartItems.stream().map(item -> {
             Product product = productClient.getProductById(item.getProductId());
 
             if (product == null || product.getStock() < item.getQuantity()) {
-                throw new ProductOutOfStockException("Invalid product or insufficient stock");
+                log.error("Failed to create order - insufficient stock");
+                throw new ProductOutOfStockException("Failed to create order - insufficient stock");
             }
 
             return BigDecimal.valueOf(product.getPrice())
@@ -97,6 +118,7 @@ public class OrderService {
     }
 
     private Order createOrderObject(BigDecimal totalAmount, BigDecimal deliveryCost, Long customerId, List<CartItem> cartItems) {
+        log.debug("Creating order object...");
         return Order.builder()
                 .orderDate(LocalDate.now())
                 .status("Pending")
