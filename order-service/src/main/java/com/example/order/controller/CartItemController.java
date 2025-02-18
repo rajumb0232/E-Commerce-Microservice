eb6b138afc7d7860c002ffbf5b5265b1c1118e01
rajumb0232 <rajugowda0232@gmail.com>
2025-02-18 14:15:05 +0530

    Updating Add to Cart logic to CartItemResponse

--- order-service/src/main/java/com/example/order/controller/CartItemController.java
+++ order-service/src/main/java/com/example/order/controller/CartItemController.java
@@ -1,5 +1,6 @@
 package com.example.order.controller;
 
+import com.example.order.dto.response.CartItemResponse;
 import com.example.order.model.CartItem;
 import com.example.order.service.CartItemService;
 import lombok.AllArgsConstructor;
@@ -15,9 +16,9 @@ public class CartItemController {
     private final CartItemService cartItemService;
 
     @PostMapping("/cart-items/products/{productId}")
-    public ResponseEntity<CartItem> createCartItem(@PathVariable Long productId, @RequestParam int quantity) {
-        CartItem cartItem = cartItemService.createCartItem(productId, quantity);
-        return ResponseEntity.status(HttpStatus.CREATED).body(cartItem);
+    public ResponseEntity<CartItemResponse> createCartItem(@PathVariable Long productId, @RequestParam int quantity) {
+        CartItemResponse response = cartItemService.createCartItem(productId, quantity);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @PatchMapping("/cart-items/{cartItemId}")
