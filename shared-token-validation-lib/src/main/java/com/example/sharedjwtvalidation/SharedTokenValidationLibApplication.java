46291c9993b5e93817ecfa574d9afdbcbb82b4ef
rajumb0232 <rajugowda0232@gmail.com>
2025-04-06 14:58:19 +0530

    created a shared library project for JWT validation

---
+++ shared-token-validation-lib/src/main/java/com/example/sharedjwtvalidation/SharedTokenValidationLibApplication.java
@@ -0,0 +1,13 @@
+package com.example.sharedjwtvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedTokenValidationLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedTokenValidationLibApplication.class, args);
+	}
+
+}
