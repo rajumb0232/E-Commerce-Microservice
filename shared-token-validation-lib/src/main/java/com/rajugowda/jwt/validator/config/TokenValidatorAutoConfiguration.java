86afa5ea06566e115b8072d09015f1870c724a37
rajumb0232 <rajugowda0232@gmail.com>
2025-04-07 18:37:34 +0530

    replaced print statement with log statement

--- shared-token-validation-lib/src/main/java/com/rajugowda/jwt/validator/config/TokenValidatorAutoConfiguration.java
+++ shared-token-validation-lib/src/main/java/com/rajugowda/jwt/validator/config/TokenValidatorAutoConfiguration.java
@@ -1,16 +1,18 @@
 package com.rajugowda.jwt.validator.config;
 
 import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "com.rajugowda.jwt.validator")
+@Slf4j
 public class TokenValidatorAutoConfiguration {
 
     @PostConstruct
     public void logInit() {
-        System.out.println("✅ TokenValidatorAutoConfiguration loaded");
+        log.info("✅ Shared Token Validation Lib AutoConfigured Successfully.");
     }
 
 }
