c6694ce38c249082140561a875fc11dd8df5e4e2
rajumb0232 <rajugowda0232@gmail.com>
2025-04-08 12:27:38 +0530

    resolving issue: service require shared objects to correctly serialize using redis cache

--- user-service/src/main/java/com/example/user/infrastructure/cache/CacheName.java
+++
@@ -1,6 +0,0 @@
-package com.example.user.infrastructure.cache;
-
-public class CacheName {
-    public static final String PUBLIC_KEYS_POOL = "public-keys-pool";
-    public static final String USER_CACHE = "user-cache";
-}
--- user-service/src/main/java/com/example/user/security/jwt/secret/IssuerVault.java
+++ user-service/src/main/java/com/example/user/security/jwt/secret/IssuerVault.java
@@ -1,13 +1,11 @@
 package com.example.user.security.jwt.secret;
 
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.stereotype.Component;
 
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.util.Map;
-import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -15,21 +13,13 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 @Component
 @Getter
-public class Vault {
+public class IssuerVault {
     private PrivateKey privateKey;
     private String currentPublicKeyId;
-    private final Map<String, PublicKey> publicKeyPool = new ConcurrentHashMap<>();
 
     public void setNewPrivateKey(PrivateKey privateKey, String correspondingPublicKeyId) {
         this.privateKey = privateKey;
         this.currentPublicKeyId = correspondingPublicKeyId;
-    }
-    public PublicKey getPublicKey(String key) {
-        return publicKeyPool.getOrDefault(key, null); // You can log missing key here
-    }
-
-    public void addPublicKey(String key, PublicKey publicKey) {
-        publicKeyPool.putIfAbsent(key, publicKey); // Ensures atomicity
     }
 }
 
