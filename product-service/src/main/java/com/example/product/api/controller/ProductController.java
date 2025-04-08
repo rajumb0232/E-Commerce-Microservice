d53f31f5c2df3e8bedc68b95995135798ce83ba4
rajumb0232 <rajugowda0232@gmail.com>
2025-04-08 19:08:11 +0530

    fixed issue: was appending /products to the base URL which was already /products

--- product-service/src/main/java/com/example/product/api/controller/ProductController.java
+++ product-service/src/main/java/com/example/product/api/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.example.product.application.dto.ProductResponse;
 import com.example.product.application.service.ProductService;
 import com.example.product.shared.CustomPage;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,41 +18,41 @@ public class ProductController {
 
     private final ProductService productService;
 
-    @PostMapping("/products")
+    @PostMapping
     public ResponseEntity<ProductResponse> addProduct(@RequestBody ProductRequest productRequest, @RequestParam Long sellerId) {
         ProductResponse productResponse = productService.addProduct(productRequest, sellerId);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(productResponse);
     }
 
-    @GetMapping("/products/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<ProductResponse> getProductById(@PathVariable Long id) {
         ProductResponse productResponse = productService.getProductById(id);
         return ResponseEntity.ok(productResponse);
     }
 
-    @GetMapping("/products")
+    @GetMapping
     public ResponseEntity<CustomPage<ProductResponse>> getAllProducts(@RequestParam int page, @RequestParam int size) {
         Page<ProductResponse> pageResponse = productService.getAllProducts(page, size);
         CustomPage<ProductResponse> responses = productService.convertToCustomPage(pageResponse);
         return ResponseEntity.ok(responses);
     }
 
-    @PutMapping("/products/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<ProductResponse> updateProduct(@PathVariable Long id, @RequestBody ProductRequest productRequest) {
         ProductResponse productResponse = productService.updateProduct(id, productRequest);
         return ResponseEntity.status(HttpStatus.OK)
                 .body(productResponse);
     }
 
-    @DeleteMapping("/products/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
         productService.deleteProduct(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT)
                 .build();
     }
 
-    @GetMapping("/products/{id}/availability")
+    @GetMapping("/{id}/availability")
     public ResponseEntity<Boolean> checkProductAvailability(@PathVariable Long id, @RequestParam int quantity) {
         boolean response = productService.checkProductAvailability(id, quantity);
         return ResponseEntity.ok(response); // Placeholder for actual implementation
